25f67ea914d50c8be6fe333bf93d30ddea334357
Edgard <20133252@ealoe.ulima.edu.pe>
2022-08-03 13:05:39 -0500

    Initial Commit

---
+++ SwiftUiLearningProject/SwiftUiLearningProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiLearningProjectApp.swift
+//  SwiftUiLearningProject
+//
+//  Created by EdgardVS on 3/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiLearningProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
